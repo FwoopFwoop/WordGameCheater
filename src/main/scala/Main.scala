17f2abdf61c55d36a56ae45642a858138e4168a9
FwoopFwoop <zacharysamiyoussef@gmail.com>
2018-01-13 12:33:45 -0500

    Add help option

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,7 +6,7 @@ object Main {
 
   object Flag extends Enumeration {
     type Flag = Value
-    val Scrabble = Value
+    val Scrabble, Help = Value
   }
 
   def sanitize(letters: String): Vector[Char] = {
@@ -27,22 +27,34 @@ object Main {
       if (arg.equalsIgnoreCase("-s")
               || arg.equalsIgnoreCase("--scrabble")) {
         Some(Flag.Scrabble)
+      } else if (arg.equalsIgnoreCase("-h")
+              || arg.equalsIgnoreCase("--help")
+              || arg.equalsIgnoreCase("help")) {
+        Some(Flag.Help)
       } else None
     }
 
-    if (!args.isEmpty && !args.tail.isEmpty) {
-      Some(args.toVector.tail.flatMap(flagFromString))
+    if (!args.isEmpty) {
+      Some(args.toVector.flatMap(flagFromString))
     } else None
   }
 
   def main(args: Array[String]): Unit = {
     val letters = parseLetters(args)
-    val flags = parseFlags(args) match{
+    val flags = parseFlags(args) match {
       case None => Vector()
       case Some(v: Vector[Flag]) => v
     }
 
-    if (flags.contains(Flag.Scrabble)) {
+    if (flags.contains(Flag.Help)) {
+      println("Arguments usage: ")
+      println("The first argument should be the letters you want to use, ")
+      println("written as one word (for example, if you have 'a, b, d, x'")
+      println("You write the argument as 'abdx')")
+      println()
+      println("Following your letters with -s or --scrabble will sort")
+      println("the words by scrabble score.")
+    } else if (flags.contains(Flag.Scrabble)) {
       Sorter.sortScrabble(Finder.matchingWords(letters)).foreach(println)
     } else {
       Sorter.sortLenAlpha(Finder.matchingWords(letters)).foreach(println)
